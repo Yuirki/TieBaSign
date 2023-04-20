4987a682a774649d5c1c8852972df74caabb0763
Yuirki <51293718+Yuirki@users.noreply.github.com>
2023-04-20 17:37:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -217,7 +217,7 @@ def main():
         return
     b = ENV['BDUSS'].split('#')
     for n, i in enumerate(b):
-        logger.info("开始签到第" + str(n) + "个用户" + i)
+        logger.info("开始签到第" + str(n+1) + "个用户")
         tbs = get_tbs(i)
         favorites = get_favorite(i)
         for j in favorites:
